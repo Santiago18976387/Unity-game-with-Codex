7a344fb837ad0b33ba65596518c75fc6181940b7
Santiago18976387 <cantosssanti1@gmail.com>
2026-05-18 20:14:29 -0500

    Update OstiasMovement to support horizontal movement

---
+++ codex/Assets/Scripts/OstiasMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class OstiasMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    private void Update()
+    {
+        float horizontalInput = 0f;
+        float verticalInput = 0f;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            horizontalInput = -1f;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            horizontalInput = 1f;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            verticalInput = 1f;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            verticalInput = -1f;
+        }
+
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f) * (speed * Time.deltaTime);
+        transform.Translate(movement);
+    }
+}
